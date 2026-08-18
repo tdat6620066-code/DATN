2877a8b8e4a74cf77be5d3aea61a9ea1cd293d9a
Quang Minh <minh5541abc@gmail.com>
2026-08-18 09:10:50 +0700

    migration user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,14 +15,13 @@ return new class extends Migration
 
             $table->string('email')->unique();
 
-            $table->string('phone', 20)->nullable()->unique();
+            $table->string('phone', 20)->unique();
 
             $table->timestamp('email_verified_at')->nullable();
-<<<<<<< HEAD
 
             $table->timestamp('phone_verified_at')->nullable();
 
-            $table->string('password')->nullable();
+            $table->string('password');
 
             $table->enum('role', [
                 'CUSTOMER',
@@ -42,13 +41,6 @@ return new class extends Migration
 
             $table->timestamp('last_login_at')->nullable();
 
-=======
-            $table->string('password');
-            $table->enum('role', ['CUSTOMER', 'EMPLOYEE', 'ADMIN'])->default('CUSTOMER');
-            $table->string('phone', 30)->nullable();
-            $table->enum('status', ['ACTIVE', 'LOCKED'])->default('ACTIVE');
-            $table->json('permissions')->nullable();
->>>>>>> 9790fd584874111b4e4d91e45e981ae25b3deaae
             $table->rememberToken();
 
             $table->timestamps();
